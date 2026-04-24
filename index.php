62ffb2b5cc9ab4011dcb5b0a020248bf5d60cfa9
jja43 <jd8asa@gmail.com>
2026-04-24 15:30:40 +0100

    actually work

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
-
 <?php
-    $date = new DateTimeImmutable('');
-    date_default_timezone_set('UTC');
-    print('logged in at ');
-    ?>
 
-    <span id="clock-server" data-ts="<?php echo $date->getTimestamp(); ?>">
-      <?php echo $date->format('Y-m-d H:i:s'); ?>
-    </span>
 
-    <?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  if (isset($_POST["firstname"]) && isset($_POST["lastname"]) && isset($_POST["comment"])) {
+    
+    $firstName = trim($_POST['firstname']);
+    $lastName = trim($_POST['lastname']);
+    $comment = trim($_POST['comment']);
 
     $servername = "localhost";
     $username = "jja43_comment";
@@ -17,64 +14,67 @@
     $dbname = "jja43_art";
 
     $conn = new mysqli($servername, $username, $password, $dbname);
-   try {
-    if ($conn->connect_error) {
+    try {
+      if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
+      }
+      echo "Connected successfully";
+    } catch (Exception $e) {
+      echo "Connection failed: " . $e->getMessage();
     }
-    echo "Connected successfully";
-   } catch (Exception $e) {
-    echo "Connection failed: " . $e->getMessage();
-   }
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {   
-if (isset($_POST["firstname"]) && isset($_POST["lastname"]) && isset($_POST["comment"])) {
-
-   $firstName = trim($_POST['firstname']);
-    $lastName  = trim($_POST['lastname']);
-    $comment   = trim($_POST['comment']);
 
 
-} else {
-   die ("Error: Missing required fields.");
-  }
-}
-  ?>
 
-<?php
-  $sql = " INSERT INTO people (firstname, lastname, comment) VALUES (? ,?, ?) ";
+    $sql = " INSERT INTO people (firstname, lastname, comment) VALUES (? ,?, ?) ";
 
-  $stmt = $conn->prepare($sql);
-   if (!$stmt) { die('Prepare failed: '.$conn->error); }
-   $stmt->bind_param("sss", $firstName, $lastName, $comment);
-   $result = $stmt->execute();
+    $stmt = $conn->prepare($sql);
+    if (!$stmt) {
+      die('Prepare failed: ' . $conn->error);
+    }
+    $stmt->bind_param("sss", $firstName, $lastName, $comment);
+    $result = $stmt->execute();
 
 
     if ($result === false) {
-        throw new Exception("Error: " . $stmt->error);
+      throw new Exception("Error: " . $stmt->error);
     }
-   //$stmt = $conn->prepare($sql);
- // $stmt->bind_param("isss", $ID, $firstName, $lastName, $comment);
- // $result = $stmt->execute();
- try {
-    if ($result === false){
+    //$stmt = $conn->prepare($sql);
+// $stmt->bind_param("isss", $ID, $firstName, $lastName, $comment);
+// $result = $stmt->execute();
+    try {
+      if ($result === false) {
         throw new Exception("Error:cant get results " . $stmt->error);
+      }
+    } catch (Exception $e) {
+      echo "Error: " . $e->getMessage();
     }
- } catch (Exception $e) {
-    echo "Error: " . $e->getMessage();
- }
+
+
+  } else {
+    http_response_code(400);
+  }
+} else {
+  http_response_code(405);
+}
+
 ?>
 
+
+<!--
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="utf-8">
-    <title>Index of examples</title>
+  <meta charset="utf-8">
+  <title>Index of examples</title>
 </head>
 
 <body>
-    <h1>Comments</h1>
+  <h1>Comments</h1>
 
 
 </body>
 <script src="script.js"></script>
 <link rel="stylesheet" type="text/css" href="first.css">
-</html>
+
+</html>-->
